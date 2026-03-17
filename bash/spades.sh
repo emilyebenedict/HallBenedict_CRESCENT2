a2dea5ab2a136a1c4910c104b04cadb64ef16298
Emily Benedict <117936813+emilyebenedict@users.noreply.github.com>
2026-03-17 15:24:27 -0500

    Create spades.sh

---
+++ bash/spades.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#===============================================================================
+# File Name    : spades.sh
+# Usage        : sbatch spades.sh
+# Author       : Lindsey Hall
+# Version      : 1.0
+# Created On   : 23-05-08
+#===============================================================================
+#Submission script for HTCF
+#SBATCH --job-name=spades
+#SBATCH --array=1-3
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=40G
+#SBATCH --output=slurm_out/spades/x_spades_%a.out
+#SBATCH --error=slurm_out/spades/y_spades_%a.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ebenedict@wustl.edu
+
+eval $(spack load --sh spades@3.15.3)
+
+#module load spades/3.14.0-python-2.7.15
+
+basedir="$PWD"
+indir="${basedir}/d01_clean_reads"
+outdir="${basedir}/d03_spades"
+
+mkdir -p ${outdir}
+
+#sample list (no extensions)
+sample=`sed -n ${SLURM_ARRAY_TASK_ID}p ${basedir}/reseq_sr_list.txt`
+
+set -x
+
+#run spades for each sample
+spades.py \
+  --isolate  \
+  -1 ${indir}/${sample}_FW_clean.fastq.gz \
+  -2 ${indir}/${sample}_RV_clean.fastq.gz \
+  -t ${SLURM_CPUS_PER_TASK}  \
+  --tmp-dir /tmp/${sample}_tmp \
+  -o ${outdir}/${sample}
+
+RC=$?
+
+set +x
+
+if [ $RC -eq 0 ]
+then
+  echo "Job completed successfully!"
+else
+  echo "Error occured for Sample ${sample}"
+  exit $RC
+fi
